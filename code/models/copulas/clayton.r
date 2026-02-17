0490ca0ce749d6cb16b34a2f89782a930543f3ce
AndreFerrero <andrea.ferrero101201@gmail.com>
2026-02-17 13:55:00 +0100

    refactor main by moving old model code to joint_copula branch

--- code/models/copulas/clayton.r
+++ code/models/copulas/clayton.r
@@ -8,55 +8,7 @@ copula_clayton <- list(
 
   name = "clayton",
 
-  # --------------------------
-  # 1. Simulate uniforms using latent gamma variable
-  # --------------------------
-  # Clayton copula CDF: C(u1,...,un) = (sum(u_i^-theta - 1) + 1)^(-1/theta)
-  # Latent variable V ~ Gamma(1/theta, 1)
-  # Then U_i = (1 + E_i / V)^(-1/theta), E_i ~ Exp(1)
-  simulate = function(theta, n) {
-
-  # --- Independence case ---
-  if (abs(theta) < 1e-8) {
-    return(runif(n))
-  }
-
-  # --- Invalid parameter ---
-  if (theta < 0) return(NULL)
-
-  # --- Clayton Archimedean simulation ---
-  V <- rgamma(n = 1, shape = 1 / theta, rate = 1)
-  if (!is.finite(V) || V <= 0) return(NULL)
-
-  E <- rexp(n)
-  (1 + E / V)^(-1 / theta)
-},
-
-  # --------------------------
-  # 2. Log-density of the Clayton copula
-  # --------------------------
-  lpdf = function(u, theta) {
-    copula::dCopula(u, copula::claytonCopula(theta, dim = length(u)), log = TRUE)
-  },
-
-  # --------------------------
-  # 3. Log-prior for the copula parameter
-  # --------------------------
-  log_prior = function(theta, a = 2, b = 1) {
-    if (theta <= 0) return(-Inf)
-    dgamma(theta, shape = a, rate = b, log = TRUE)
-  },
-
-  diag = function(u, theta) {
-    n <- length(u)
-    
-    if (theta < 0) return(NA)             # invalid parameter
-    if (theta == 0) return(u^n)           # independence case
-    
-    (n * (u^(-theta) - 1) + 1)^(-1/theta)
-  },
-
-    # P(U <= u | V = v) = v^(-theta-1) * (u^-theta + v^-theta - 1)^(-1/theta - 1)
+  # P(U <= u | V = v) = v^(-theta-1) * (u^-theta + v^-theta - 1)^(-1/theta - 1)
   h_dist = function(u, v, theta) {
     if (abs(theta) < 1e-8) return(u) # Independence case
     
--- code/models/copulas/gumbel.r
+++ code/models/copulas/gumbel.r
@@ -9,51 +9,6 @@ copula_gumbel <- list(
   inv_psi = function(u, theta) (-log(u))^theta,
   psi = function(t, theta) exp(-t^(1/theta)),
 
-  # --------------------------
-  # 1. Simulate uniforms using latent variable method
-  # --------------------------
-  simulate = function(theta, n) {
-    if (theta == 1) return(runif(n))  # independence
-    if (theta < 1) return(NULL)       # invalid
-
-    # --- simulate latent variable V ---
-    val_gamma <- (cos(pi / (2 * theta)))^theta
-    V <- stabledist::rstable(
-      n = 1,
-      alpha = 1 / theta,
-      beta  = 1,
-      gamma = val_gamma,
-      delta = 0,
-      pm    = 1
-    )
-
-    if (!is.finite(V) || V <= 0) return(NULL)
-
-    # --- exponential variates ---
-    E <- rexp(n)
-
-    # --- apply inverse generator psi ---
-    U <- copula_gumbel$psi(E / V, theta)
-    U
-  },
-
-  lpdf = function(u, theta) {
-    copula::dCopula(u, copula::gumbelCopula(theta, dim = length(u)), log = TRUE)
-  },
-
-  log_prior = function(theta, a = 2, b = 1) {
-    if (theta <= 1) return(-Inf)
-    dgamma(theta - 1, a, b, log = TRUE)
-  },
-
-  diag = function(u, theta, n = length(u)) {
-    
-    if (theta < 1) return(NA)             # invalid parameter
-    if (theta == 1) return(u^n)           # independence case
-    
-    u^(n^(1/theta))
-  },
-
   # Gumbel h-function (Conditional CDF) for Markov transitions
 
   h_dist = function(u, v, theta) {
