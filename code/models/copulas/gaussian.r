a02f032bb098496b43327674e50b434b2084ef34
AndreFerrero <andrea.ferrero101201@gmail.com>
2026-02-24 11:06:27 +0100

    added cdf and diagonal

--- code/models/copulas/gaussian.r
+++ code/models/copulas/gaussian.r
@@ -1,28 +1,75 @@
-# ==============================================================
-# Gaussian Copula Object
-# ==============================================================
 copula_gaussian <- list(
   name = "gaussian",
+  name_param = "rho",
   
-  # The h-function: P(U <= u | V = v)
-  h_dist = function(u, v, rho) {
-    # Standardize inputs to prevent Inf in qnorm
+  # -------------------------------------------------
+  # Joint CDF: C(u,v) = Phi_2(qnorm(u), qnorm(v); rho)
+  # -------------------------------------------------
+  cdf = function(u, v, rho) {
+    
     u <- pmax(pmin(u, 1 - 1e-12), 1e-12)
     v <- pmax(pmin(v, 1 - 1e-12), 1e-12)
     
-    # Gaussian conditional formula
-    # x = qnorm(u), y = qnorm(v)
-    # P(X <= x | Y = y) = Phi( (x - rho*y) / sqrt(1 - rho^2) )
-    res <- pnorm( (qnorm(u) - rho * qnorm(v)) / sqrt(1 - rho^2) )
+    x <- qnorm(u)
+    y <- qnorm(v)
+    
+    # Vectorised evaluation
+    res <- mapply(function(xi, yi) {
+      mvtnorm::pmvnorm(
+        lower = c(-Inf, -Inf),
+        upper = c(xi, yi),
+        sigma = matrix(c(1, rho, rho, 1), 2, 2)
+      )[1]
+    }, x, y)
+    
+    return(pmin(pmax(res, 0), 1))
+  },
+  
+  # -------------------------------------------------
+  # Diagonal CDF: C(u,u)
+  # -------------------------------------------------
+  cdf_diag = function(u, rho) {
+    
+    u <- pmax(pmin(u, 1 - 1e-12), 1e-12)
+    x <- qnorm(u)
+    
+    res <- sapply(x, function(xi) {
+      mvtnorm::pmvnorm(
+        lower = c(-Inf, -Inf),
+        upper = c(xi, xi),
+        sigma = matrix(c(1, rho, rho, 1), 2, 2)
+      )[1]
+    })
+    
+    return(pmin(pmax(res, 0), 1))
+  },
+  
+  # -------------------------------------------------
+  # Conditional CDF h-function
+  # -------------------------------------------------
+  h_dist = function(u, v, rho) {
+    u <- pmax(pmin(u, 1 - 1e-12), 1e-12)
+    v <- pmax(pmin(v, 1 - 1e-12), 1e-12)
+    
+    res <- pnorm(
+      (qnorm(u) - rho * qnorm(v)) /
+      sqrt(1 - rho^2)
+    )
+    
     return(res)
   },
   
-  # The actual inverse h-function (for speed, though bisection works too)
+  # -------------------------------------------------
+  # Inverse h-function
+  # -------------------------------------------------
   h_inv = function(w, v, rho) {
     v <- pmax(pmin(v, 1 - 1e-12), 1e-12)
-    # Solving h(u|v) = w for u:
-    # u = Phi( rho * Phi^-1(v) + sqrt(1 - rho^2) * Phi^-1(w) )
-    u <- pnorm( rho * qnorm(v) + sqrt(1 - rho^2) * qnorm(w) )
+    
+    u <- pnorm(
+      rho * qnorm(v) +
+      sqrt(1 - rho^2) * qnorm(w)
+    )
+    
     return(u)
   }
 )
