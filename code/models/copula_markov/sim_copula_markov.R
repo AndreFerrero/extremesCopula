2f5e7bfd17f1afbf6a36e93fc3bec236c04fdf99
AndreFerrero <andrea.ferrero101201@gmail.com>
2026-02-24 11:06:03 +0100

    formatting

--- code/models/copula_markov/sim_copula_markov.R
+++ code/models/copula_markov/sim_copula_markov.R
@@ -1,19 +1,24 @@
 # Optimized Markov Simulation Engine
-simulate_copula_markov <- function(n, copula, copula_param, margin, margin_param, burn = 100, bisec_it = 20) {
+simulate_copula_markov <- function(
+  n,
+  copula, copula_param,
+  margin, margin_param,
+  burn = 100, bisec_it = 20
+) {
   total_n <- n + burn
-  U <- numeric(total_n)
-  U[1] <- runif(1, 1e-5, 1 - 1e-5)
+  u <- numeric(total_n)
+  u[1] <- runif(1, 1e-5, 1 - 1e-5)
 
   # Check if the copula object has a pre-defined analytical inverse
   has_h_inv <- !is.null(copula$h_inv)
 
   for (t in 2:total_n) {
-    v_prev <- U[t - 1]
+    v_prev <- u[t - 1]
     w_target <- runif(1)
-    
+
     if (has_h_inv) {
       # FAST PATH: Use analytical inversion (e.g., Gaussian, Clayton)
-      U[t] <- copula$h_inv(w_target, v_prev, copula_param)
+      u[t] <- copula$h_inv(w_target, v_prev, copula_param)
     } else {
       # ROBUST PATH: Fallback to Bisection (e.g., Gumbel, Joe)
       low <- 1e-10
@@ -22,12 +27,12 @@ simulate_copula_markov <- function(n, copula, copula_param, margin, margin_param
         mid <- (low + high) / 2
         if (copula$h_dist(mid, v_prev, copula_param) < w_target) low <- mid else high <- mid
       }
-      U[t] <- (low + high) / 2
+      u[t] <- (low + high) / 2
     }
   }
-  
-  U_final <- U[(burn + 1):total_n]
-  X <- margin$quantile(U_final, margin_param)
-  
-  return(list(X = X, U = U_final, method_used = ifelse(has_h_inv, "analytical", "bisection")))
+
+  u_final <- u[(burn + 1):total_n]
+  x <- margin$quantile(u_final, margin_param)
+
+  return(list(x = x, u = u_final, method_used = ifelse(has_h_inv, "analytical", "bisection")))
 }
