fb13ee9f54aff728e293de92a9b3d44d862103b6
AndreFerrero <andrea.ferrero101201@gmail.com>
2026-02-24 11:07:04 +0100

    joe cdf

--- code/models/copulas/joe.r
+++ code/models/copulas/joe.r
@@ -3,7 +3,42 @@
 # ------------------------------
 copula_joe <- list(
   name = "joe",
+  name_param = "theta",
+  # -------------------------------------------------
+  # Joint CDF
+  # -------------------------------------------------
+  cdf = function(u, v, theta){
 
+    u <- pmax(pmin(u, 1 - 1e-12), 1e-12)
+    v <- pmax(pmin(v, 1 - 1e-12), 1e-12)
+
+    a <- (1 - u)^theta
+    b <- (1 - v)^theta
+
+    K <- a + b - a*b
+    K <- pmax(K, 1e-15)
+
+    C <- 1 - K^(1/theta)
+
+    return(pmin(pmax(C,0),1))
+  },
+
+  # -------------------------------------------------
+  # Diagonal CDF C(u,u)
+  # -------------------------------------------------
+  cdf_diag = function(u, theta){
+
+    u <- pmax(pmin(u, 1 - 1e-12), 1e-12)
+
+    t <- (1-u)^theta
+
+    K <- 2*t - t^2
+    K <- pmax(K, 1e-15)
+
+    C <- 1 - K^(1/theta)
+
+    return(pmin(pmax(C,0),1))
+  },
   # --------------------------
   # h-function: P(U <= u | V = v)
   # Derived as: K^(1/theta - 1) * (1-v)^(theta-1) * (1 - (1-u)^theta)
