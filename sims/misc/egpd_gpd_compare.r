2b5fe2d0b809ccc32a2630a33ad578852192fe33
AndreFerrero <andrea.ferrero101201@gmail.com>
2026-03-26 10:30:57 +0100

    normal experiment

--- sims/misc/egpd_gpd_compare.r
+++ sims/misc/egpd_gpd_compare.r
@@ -7,12 +7,13 @@ source("code/models/margins/egp.r")
 
 x <- margin_egp$sample(100, c(mu = 0, kappa = 1.5, sigma = 2, xi = 0.2))
 y <- rfrechet(100, scale = 1, shape = 4)
+x_norm <- rnorm(1e6, mean = 4, sd = 1)
 
 p <- 0.85
-plot(x)
-abline(h = quantile(x, p), col = "red", lty = 2) # Threshold for GPD fit
-sum(x > quantile(x, p)) # Number of exceedances
-hist(x[x > quantile(x, p)], breaks = 20, main = "Exceedances over 85th Percentile", xlab = "Exceedance Value")
+plot(x_norm)
+abline(h = quantile(x_norm, p), col = "red", lty = 2) # Threshold for GPD fit
+sum(x_norm > quantile(x_norm, p)) # Number of exceedances
+hist(x_norm[x_norm > quantile(x_norm, p)], breaks = 20, main = "Exceedances over 85th Percentile", xlab = "Exceedance Value")
 
 plot(y)
 abline(h = quantile(y, p), col = "red", lty = 2) # Threshold for GPD fit
@@ -21,8 +22,8 @@ hist(y[y > quantile(y, p)], breaks = 20, main = "Exceedances over 95th Percentil
 
 # 1. Fit the EGPD to ALL data (Global Fit)
 # 'model = 1' in mev corresponds to your [GPD]^kappa model
-fit_egpd <- fit.extgp(x,
-    model = 1, init = list(kappa = 1, sigma = sd(x), xi = 0.1),
+fit_egpd <- fit.extgp(x_norm,
+    model = 1, init = list(kappa = 1, sigma = sd(x_norm), xi = 0.1),
     method = "mle"
 )
 
@@ -32,8 +33,8 @@ params_egpd <- fit_egpd$fit$mle
 
 # 2. Fit a standard GPD to the TAIL only (Benchmark)
 # Pick a high threshold (e.g., 95th percentile)
-u <- quantile(x, p)
-fit_gpd <- fevd(x, threshold = u, type = "GP", method = "MLE")
+u <- quantile(x_norm, p)
+fit_gpd <- fevd(x_norm, threshold = u, type = "GP", method = "MLE")
 params_gpd <- list(sigma = fit_gpd$results$par[1], xi = fit_gpd$results$par[2])
 
 plot(fit_gpd)
@@ -56,7 +57,7 @@ z_egpd <-(params_egpd["sigma"] / params_egpd["xi"]) *
 
 # GPD Return Level (standard POT formula)
 # Note: zeta_u is the probability of being above the threshold
-zeta_u <- mean(x > u)
+zeta_u <- mean(x_norm > u)
 z_gpd <- u + (params_gpd$sigma / params_gpd$xi) *
     ((((r * ny) * zeta_u)^params_gpd$xi) - 1)
 
