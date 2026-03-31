c803c3ad870fa17915a844f54a609adb42bf3504
AndreFerrero <andrea.ferrero101201@gmail.com>
2026-03-31 10:14:11 +0200

    removed title on lag plot func

--- code/handy_funs.r
+++ code/handy_funs.r
@@ -3,18 +3,13 @@ block_max <- function(x, block_size) {
   sapply(split(x, ceiling(seq_along(x) / block_size)), max)
 }
 
-lag_plot <- function(x) {
+lag_plot <- function(x, main = NULL) {
   n <- length(x)
-  
-  # Get the name of the object passed
-  varname <- deparse(substitute(x))
   
   plot(
     x[-n], x[-1],
     pch = 16,
-    main = paste("Lag plot of", varname),
-    xlab = bquote(.(as.name(varname))[t-1]),
-    ylab = bquote(.(as.name(varname))[t])
+    main = main,
   )
   
   abline(0, 1, col = "red")
