322b0f6f40dccd995900adcd96f46a6fb7ea2b38
AndreFerrero <andrea.ferrero101201@gmail.com>
2026-05-11 09:35:19 +0200

    fixed bug

--- code/models/hill_estimator.r
+++ code/models/hill_estimator.r
@@ -1,13 +1,16 @@
 # --- Hill Estimator and Bias-Corrected Hill ---
-source("code/models/copula_markov/simulate.r")
+#' @param data A positive numeric vector of data points.
+#' @param k_target The number of top order statistics to use
 
 hill_bc_hat <- function(data, k_target) {
+  # data is a positive vector
+
+  n <- length(data)
   order_stats <- sort(data, decreasing = TRUE)
 
   calc_all_moments <- function(sorted_data, k) {
-    n <- length(sorted_data)
-    top_k <- sorted_data[(n - k + 1):n]
-    threshold <- sorted_data[n - k]
+    top_k <- sorted_data[1:k]
+    threshold <- sorted_data[k+1]
     log_excess <- log(top_k) - log(threshold)
 
     c(
@@ -30,14 +33,9 @@ hill_bc_hat <- function(data, k_target) {
     (-4 + 6 * Sk2 + sqrt(3 * Sk2 - 2)) / (4 * Sk2 - 3)
   }
 
-  m <- length(data[data > 0])
-  if (m < 20) {
-    return(NA)
-  }
-
   # 1. Estimate Rho using your Supremum logic
-  search_upper <- floor(min(m - 1, (2 * m) / log(log(m))))
-  search_range <- seq(search_upper, 10, by = -1)
+  search_upper <- floor(min(n - 1, (2 * n) / log(log(n))))
+  search_range <- seq(search_upper, 1, by = -1)
 
   rho_star <- NA
   for (k_val in search_range) {
@@ -54,6 +52,8 @@ hill_bc_hat <- function(data, k_target) {
     return(NA)
   }
 
+  print(paste("Estimated Rho:", round(rho_star, 4), "at k =", k_val)
+  )
   M <- calc_all_moments(order_stats, k_target) # Moments at k_target
   
   # 2. Apply correction at the specific k_target
@@ -66,5 +66,7 @@ hill_bc_hat <- function(data, k_target) {
   term_rho <- rho_star / (1 - rho_star)
   bias_corr <- (m2 - 2 * gamma_h^2) / (2 * gamma_h * term_rho)
 
-  list(hill = gamma_h, hill_bc = gamma_h - bias_corr)
+  xi_hat <- c(gamma_h, gamma_h - bias_corr)
+  names(xi_hat) <- c("Hill", "Hill_BC")
+  return(xi_hat)
 }
